d6fab9c3cbf399a20a3d61e75daad04f8f29e70c
Yovanny Nogales <nverayovanny@gmail.com>
2018-10-28 13:20:54 -0700

    StorageCSV.cs
    Added Read, Write, WriteLine and Reset function in the StorageCSV class.

---
+++ Assets/Scripts/Utils/Storage/StorageCSV.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using Entities.Utils;
+
+namespace Entities.Utils.Storage
+{
+    public static class StorageCSV
+    {
+        public static void Save (string filename, List<string[]> data)
+        {
+            StreamWriter CSVFile = File.CreateText(Functions.GetPath("Resources", filename));
+            for (int c = 0; c < data.Count; c++)
+            {
+                if (c == 0)
+                {
+                    CSVFile.Write(string.Join(",", data[c]));
+                } else
+                {
+                    CSVFile.Write("\n" + string.Join(",", data[c]));
+                }
+            }
+            CSVFile.Close();
+            Debug.Log("The file was stored succesfully!!!!");
+        }
+
+        public static void SaveNewRow(string filename, string[] row)
+        {
+            StreamWriter CSVFile = new StreamWriter(Functions.GetPath("Resources", filename), true);
+            CSVFile.Write("\n" + string.Join(",", row));
+            CSVFile.Close();
+            Debug.Log("The file was updated succesfully!!!!");
+        }
+
+        public static List<string[]> Load (string filename)
+        {
+            string[] data;
+            List<string[]> dataResult = new List<string[]>();
+            TextAsset CSVFile = Resources.Load<TextAsset>(filename);
+            data = CSVFile.text.Split(new char[] { '\n' });
+
+            foreach (var row in data)
+            {
+                string[] fields = row.Split(new char[] { ',' });
+                dataResult.Add(fields);
+            }
+            dataResult.RemoveAt(0);
+            return dataResult;
+        }
+
+        public static void ResetCSV (string filename, string[] fields)
+        {
+            StreamWriter CSVFile = File.CreateText(Functions.GetPath("Resources", filename));
+            CSVFile.Write(string.Join(",", fields));
+            CSVFile.Close();
+            Debug.Log("The file was reseted succesfully!!!!");
+        }
+    }
+}
